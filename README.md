7d66a16620cadf8c39571d746ec2351d35b90d4c
ivanbrennan <ivan.brennan@gmail.com>
2017-11-24 00:14:21 -0500

    add init.sh
    I considered just having README instructions to git clone the repo and
    manually run the appropriate script from within it, but decided to go
    with a single bootstrapable script since it guarantees the clone
    destination and is just generally a nice approach when feasible.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-even nixos needs some dotfiles
+### Install
+
+``` sh
+bash -c "$(curl -fsSL https://api.github.com/repos/ivanbrennan/nixdots/contents/init.sh \
+                -H 'Accept: application/vnd.github-blob.raw')" \
+     'init.sh' --github-host github.com
+```
+If you're using an SSH config file to manage multiple identities, you can use the `--github-host` flag to specify the appropriate Host.
---
+++ init.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -eu
+
+{ # Prevent execution if this script was only partially downloaded
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -g|--github-host)
+      arg_github_host="$2"
+      shift
+      ;;
+    *)
+      printf 'Usage: bash %s [-g|--github-host HOST]\n' "$0"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+github_host="${arg_github_host:-github.com}"
+dotfiles="$HOME/Development/resources/dotfiles"
+
+if [ ! -d "$dotfiles" ]; then
+  mkdir -p "$dotfiles"
+
+  git clone \
+      "git@${github_host}:ivanbrennan/nixdots.git" \
+      "$dotfiles"
+
+  cd "$dotfiles"
+  git config user.email 'ivan.brennan@gmail.com'
+  git config user.name 'ivanbrennan'
+  git config core.editor 'vim'
+fi
+
+} # End of wrapping
