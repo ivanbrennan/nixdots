44df4b4ad678d64dd0eae186df6e50f0e2f4fa88
ivanbrennan <ivan.brennan@gmail.com>
2018-04-21 19:57:43 -0400

    ghci.conf

--- install/make-symlinks.sh
+++ install/make-symlinks.sh
@@ -5,37 +5,42 @@ set -eu
 repo=$DOTFILES
 
 config_files=(
-  "$repo/alacritty"
-  "$repo/git"
+    "$repo/alacritty"
+    "$repo/git"
 )
 
 main() {
-  make_config_symlinks
+    make_config_symlinks
+    make_ghci_conf_symlink
 }
 
 make_config_symlinks() {
-  for src in "${config_files[@]}"; do
-    link="$HOME/.config/$(basename "$src")"
-    make_symlink "$src" "$link"
-  done
+    for src in "${config_files[@]}"; do
+        link="$HOME/.config/$(basename "$src")"
+        make_symlink "$src" "$link"
+    done
+}
+
+make_ghci_conf_symlink() {
+    make_symlink "$repo/ghc/ghci.conf" "$HOME/.ghc/ghci.conf"
 }
 
 make_symlink() {
-  local src=$1 link=$2
+    local src=$1 link=$2
 
-  if already_exists "$link"; then
-    backup="${link}-backup-$(date +%s)"
-    echo "$link already exists. Backing up to $backup"
-    mv -i "$link" "$backup"
-  fi
+    if already_exists "$link"; then
+        backup="${link}-backup-$(date +%s)"
+        echo "$link already exists. Backing up to $backup"
+        mv -i "$link" "$backup"
+    fi
 
-  mkdir -p $(dirname "$link")
-  ln -svnf "$src" "$link" | grep --color=auto -Fe '->'
+    mkdir -p $(dirname "$link")
+    ln -svnf "$src" "$link" | grep --color=auto -Fe '->'
 }
 
 already_exists() {
-  # non-empty && not a symlink
-  [ -s "$1" ] && [ ! -L "$1" ]
+    # non-empty && not a symlink
+    [ -s "$1" ] && [ ! -L "$1" ]
 }
 
 main
