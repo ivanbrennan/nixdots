f3cf898fb3e8a2616e46aa9fc880a58f7959864b
ivanbrennan <ivan.brennan@gmail.com>
2018-10-16 07:57:24 -0400

    mutt: clean up tmp files

--- stow/mutt/.mutt/scripts/view_mail.sh
+++ stow/mutt/.mutt/scripts/view_mail.sh
@@ -5,16 +5,21 @@ tmpfile="$1"
 encoding="${2:-utf-8}"
 
 # Need to copy the file because mutt will delete it before Vim can read it.
-dir=$(mktemp -d)
-cp "$tmpfile" "$dir/preview"
-preview="$dir/preview"
+tmpDir=$(mktemp -d)
+cp "$tmpfile" "$tmpDir/preview"
+preview="$tmpDir/preview"
 
 if [ -z "$TMUX" ]; then
-  vim -MR "+set encoding=$encoding" '+set filetype=mail' '+set nofoldenable' "$preview"
+    trap "rm -rf '$tmpDir'" EXIT
+    vim -MR "+set encoding=$encoding" '+set filetype=mail' '+set nofoldenable' "$preview"
 elif [ "$columns" -gt 180 ]; then
-  tmux split-window -p 50 -h \
-    vim -MR "+set encoding=$encoding" '+set filetype=mail' '+set nofoldenable' "$preview"
+    tmux split-window -p 50 -h "
+        trap 'rm -rf \"$tmpDir\"' EXIT
+        vim -MR \"+set encoding=$encoding\" '+set filetype=mail' '+set nofoldenable' \"$preview\"
+    "
 else
-  tmux split-window -p 50 -v \
-    vim -MR "+set encoding=$encoding" '+set filetype=mail' '+set nofoldenable' "$preview"
+    tmux split-window -p 50 -v "
+        trap 'rm -rf \"$tmpDir\"' EXIT
+        vim -MR \"+set encoding=$encoding\" '+set filetype=mail' '+set nofoldenable' \"$preview\"
+    "
 fi
